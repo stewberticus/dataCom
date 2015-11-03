861669f77c2bc1a6e9785a734b05d5ed55159d4d
Your Name <you@example.com>
2015-11-02 21:31:52 -0500

    icmp

--- proj4/route2.c
+++ proj4/route2.c
@@ -395,9 +395,9 @@ int main(){
 			memcpy(&icmp->src , &icmp->dst, sizeof(icmp->src));
 			memcpy(&icmp->dst,&tmp_pnt,sizeof(tmp_pnt));
 			//icmp->dst =  (char *) tmp_pnt;
-		void * icmp_type = etherhead + 36;
+		void * icmp_type = etherhead + 40;
 		char * k = (char *) icmp_type;
-		*k = 7;
+		*k = 0;
 		send(packet_socket ,buffer,BUF_SIZE,0);
 		 	
 		
