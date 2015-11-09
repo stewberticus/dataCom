13917306454310a0fd63fc87f1e57474ea4c0f35
Stuart <stuarta@arch08.cis.gvsu.edu>
2015-11-09 16:15:16 -0500

    nothing to say

--- proj4/route2.c
+++ proj4/route2.c
@@ -450,7 +450,7 @@ int main(){
 		unsigned short answer = 0;
 		int chck_sum = 0;
 		int i = 14;
-		for(i; i<36; i++){
+		for(i; i<34; i++){
 		  if(!(i == 24 || i == 25)){
 		    printf("%x\n",*checksumhead);
 		    chck_sum = chck_sum + *checksumhead;
