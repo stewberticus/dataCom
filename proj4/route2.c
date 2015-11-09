d2c54bfd6dd1f6e0471629dd23db89cd9c809543
Stuart <stuarta@arch08.cis.gvsu.edu>
2015-11-09 16:00:41 -0500

    stuuf

--- proj4/route2.c
+++ proj4/route2.c
@@ -451,7 +451,7 @@ int main(){
 		int chck_sum = 0;
 		int i = 14;
 		for(i; i<36; i++){
-		  if(!(i == 20 && i == 21)){
+		  if(!(i == 20 || i == 21)){
 		    printf("%x\n",*checksumhead);
 		    chck_sum = chck_sum + *checksumhead;
 		    checksumhead ++;
