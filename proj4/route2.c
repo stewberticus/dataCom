b3cd31fba88b00048b10fb39dd3fcb8241e5d612
Stuart <stuarta@dc19.cis.gvsu.edu>
2015-11-09 17:51:05 -0500

    hhehe

--- proj4/route2.c
+++ proj4/route2.c
@@ -271,11 +271,7 @@ int main(){
 
     if(is_arp == 1) {
       
-		memcpy(&tmp,etherhead,sizeof(tmp));
 		
-		memcpy(etherhead,etherhead+6,sizeof(tmp));
-
-		memcpy(etherhead +6,&tmp,sizeof(tmp));
 	  
 			ah =(struct arp_header *) (etherhead+14);
 					printf("buffer is---------------- %s \n",(char*)ah);
