74793f363d666f996036ac8bc73b14d5e58df156
Stuart <stuarta@arch03.cis.gvsu.edu>
2015-11-19 12:47:46 -0500

    started udp stuff

---
+++ udp_client.py
@@ -0,0 +1,62 @@
+__author__ = 'alex.stuart,lauren.mills'
+import socket               # Import socket module
+import select
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+try:
+	host = raw_input("Enter IP address: ")
+	#host = socket.gethostname()
+	#print host
+except Exception:
+        print "not an IP address"
+        quit()                     # Reserve a port for your service.
+#host = socket.gethostname() # Get local machine name
+
+port = raw_input("Enter port number: ")
+try:
+    port = int(port)
+    # throw Exception if port < 0 or > 352983752 whatever
+    if(port < 0 or port > 65535):
+        raise Exception;
+except Exception:
+    print "Not a port number"
+    quit()                     # Reserve a port for your service.
+try:
+	s.connect((host, port))
+except Exception:
+	print "problem connecting to that IP address"
+	quit()
+while True:
+    filename = raw_input("Enter the file's name or press ENTER to exit: ")
+    b=bytes(filename)
+    if not b:
+        print "Exiting"
+        quit()
+    print 'Sending...'
+    s.send(b)
+    l = s.recv(1024)
+    if l=="*****":
+        print("file not available")
+        quit()
+    s.settimeout(1.0)
+    #print "rec 1st"
+    newfile = open(filename, 'ab')
+    while(l):
+        
+        
+        newfile.write(l)
+        #print "wrote a chunk"
+        #print l
+        try:
+            l = s.recv(1024)
+        except Exception: 
+           
+    #	newfile.write(l)
+    #	print "inexpection"
+    #	print l
+            l = False
+            pass
+#f.close()
+print "Done Recieving"
+#print s.recv(1024)
+       
---
+++ udp_server.py
@@ -0,0 +1,63 @@
+__author__ = 'alex.stuart,Lauren.mills,Caleb Stevenson'
+import socket               # Import socket module
+import thread
+
+def clientconnection(c, addr):
+    while True:
+        #receive 1024 bytes for the filename
+        l = c.recv(1024)
+        if not l:
+            break
+        #decode the raw byte using UTF8 
+        filename = l.decode("utf-8")
+        print filename
+        
+        #open the file request 
+        try:
+            file = open(filename,'r')
+        
+        #read the file from disk in 1024 byte chunks 
+            l = file.read(1024)
+        except Exception:
+            #something went wrong with the file need to tell the client 
+            c.send("*****")
+            l = False
+            file = False
+        #while bytes to send
+        while(l):
+            c.send(l)
+            #read next 1024
+            l = file.read(1024)
+        if file:
+            file.close()
+        
+        print "finished sending file"
+    print 'Client ' + str(addr) + ' is no longer connected.'
+
+serv_sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)# Create a socket object
+this_machine_name = socket.gethostname() # Get local machine ip
+#print this_machine_name
+
+port = raw_input("Enter port number: ")
+try:
+    port = int(port)
+    if(port < 0 or port > 65535):
+        raise Exception
+except Exception:
+	print "not a valid port number"
+	quit()                # Reserve the passed port for your service.
+serv_sock.bind((this_machine_name, port))        # Bind to the port
+   # Open a temp file to store the data
+serv_sock.listen(1)                 # Now wait for client connection.
+
+while True:
+    try:
+        #returns a socket and the adress we are connected to
+        c, addr = serv_sock.accept()     # Establish connection with client.
+        #print 'Connected to', addr       # Confirm correct client
+        thread.start_new_thread(clientconnection, (c, addr))
+    except Exception as e:
+        print e
+        print "Could not start thread"
+
+c.close()
